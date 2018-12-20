d6fa202161c20182df108224febb33011f88f27f
nico <nicolauria@outlook.com>
2018-12-20 09:57:49 -0800

    finish app.js server file

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+
+// start the server
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server running on port ${port}.`));
+
+// connect to mongoose db
+const mongoose = require('mongoose');
+const db = require('./config/keys').mongoURI;
+mongoose
+  .connect(db)
+  .then(() => console.log('Successfully connected to MongoDB'))
+  .catch(err => console.log(err));
+
+// parse body of post requests
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// abstract users and posts api into separate files
+const users = require('./routes/api/users');
+const posts = require('./routes/api/posts');
+app.use('/api/users', users);
+app.use('/api/posts', posts);
+
+// passport jwt authentication
+const passport = require('passport');
+require('./config/passport')(passport);
+app.use(passport.initialize());
