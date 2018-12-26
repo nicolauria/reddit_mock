1bc624d034b887885fc4eee950cbcd4d864e9144
nico <nicolauria@outlook.com>
2018-12-26 15:19:23 -0800

    remove redux chrome extension

--- client/src/store.js
+++ client/src/store.js
@@ -9,9 +9,11 @@ const store = createStore(
   rootReducer,
   {},
   compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    applyMiddleware(...middleware)
   )
 );
 
 export default store;
+
+// REDUX CHROME EXTENSION
+// window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
